6ddfb98dde40829d3664d48f9abd958d0f840e2a
Dennis Ickes <dennis@dendery.de>
2022-09-14 22:25:35 +0200

    Changed

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -3,6 +3,8 @@ module.exports = (client) => {
         if (!client.user || !client.application) {
             return;
         }
+        
+        client.user.setStatus(PresenceUpdateStatus.Idle, 'searching for food');
 
         console.log(client.user.username + ' is online');
     });
